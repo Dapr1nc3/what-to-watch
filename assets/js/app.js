18cdb10e0bd8f698e41943a641eaffa11d121872
Brian Done <briguy2078@gmail.com>
2021-11-23 15:50:28 -0500

    updated tmdb api url

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,8 @@ var getMovies = function() {
     // Documentation URL: https://developers.themoviedb.org/3/getting-started/search-and-query-for-details
     // API image documention: https://developers.themoviedb.org/3/movies/get-movie-images
     // Sample API Movie Image Request: https://api.themoviedb.org/3/movie/550/images?/7PzJdsLGlR7oW4J0J5Xcd0pHGRg.png?api_key=f7d85a51bfa2246df462b8756b8f2df8&language=en-US
-    // Sampe API Fight Club Movie Request: https://api.themoviedb.org/3/search/movie?550?api_key=f7d85a51bfa2246df462b8756b8f2df8&query=Jack+Reacher
-    var apiTmdbUrl = "https://api.themoviedb.org/3/search/movie?api_key=f7d85a51bfa2246df462b8756b8f2df8&query=Jack+Reacher";
+    // Sampe API Fight Club Movie Request: https://api.themoviedb.org/3/movie/550?api_key=f7d85a51bfa2246df462b8756b8f2df8
+    var apiTmdbUrl = "https://api.themoviedb.org/3/movie/550?api_key=f7d85a51bfa2246df462b8756b8f2df8";
 
     // Make a Request to the TMDb API Link
     fetch(apiTmdbUrl).then(function(response) {
