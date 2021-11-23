cb51796955740a0e3d452a05d37fd6a1f763d9d7
Brian Done <briguy2078@gmail.com>
2021-11-23 14:39:39 -0500

    added tmdb logo per api doc requirement, need to move logo to footer, created fetch function for tmdb api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+var getMovies = function() {
+    // The Movie Database API Link
+    // Link pulls response in json in placed into browser directly
+    // Still working on having the data show in console.log
+    // Documentation URL: https://developers.themoviedb.org/3/getting-started/search-and-query-for-details
+    var apiUrl = "https://api.themoviedb.org/3/search/movie?api_key=f7d85a51bfa2246df462b8756b8f2df8&query=Jack+Reacher";
+
+    // Make a Request to the API Link
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+}
+
 // Initialize all div with carousel class
 var carousels = bulmaCarousel.attach('.carousel');
 
@@ -21,7 +36,7 @@ if (element && element.bulmaCarousel) {
 bulmaCarousel.attach('#carousel-demo', {
     slidesToScroll: 1,
     slidesToShow: 1,
-    
+    navigationKeys: true,
     
 
 });
