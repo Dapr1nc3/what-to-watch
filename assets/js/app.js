3c63719fc064245ba9d36c5a959a4d8ce3aea8e7
Brian Done <briguy2078@gmail.com>
2021-11-23 15:24:52 -0500

    add js fetch function for youtube api with api key

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,25 @@ var getMovies = function() {
     // Still working on having the data show in console.log
     // Documentation URL: https://developers.themoviedb.org/3/getting-started/search-and-query-for-details
     // API image documention: https://developers.themoviedb.org/3/movies/get-movie-images
-    var apiUrl = "https://api.themoviedb.org/3/search/movie?api_key=f7d85a51bfa2246df462b8756b8f2df8&query=Jack+Reacher";
+    var apiTmdbUrl = "https://api.themoviedb.org/3/search/movie?api_key=f7d85a51bfa2246df462b8756b8f2df8&query=Jack+Reacher";
 
-    // Make a Request to the API Link
-    fetch(apiUrl).then(function(response) {
+    // Make a Request to the TMDb API Link
+    fetch(apiTmdbUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         })
-    })
-}
+    });
+
+    // YouTube API documention: https://developers.google.com/youtube/v3/getting-started
+    var apiYoutubeUrl = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyBwjtqScAQ_N-v_ZExCLGwF7GHHh6CLfKQ";
+
+    // Make a Request to the YouTube API Link
+    fetch(apiYoutubeUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    });
+};
 
 // Initialize all div with carousel class
 var carousels = bulmaCarousel.attach('.carousel');
