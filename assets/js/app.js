60acf00933d19d09ddef69c691b4a733b61ae4ef
camyacodes <camyacodes@gmail.com>
2021-11-24 17:42:56 -0500

    navbar hamburger and dark theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-var start
+var chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark-theme');
+});
+
+hamburgerMenu();
+
+function hamburgerMenu() {
+    var burger = document.querySelector(".burger")
+    var nav = document.querySelector("#"+burger.dataset.target)
+    burger.addEventListener('click', function(){
+        burger.classList.toggle('is-active');
+        nav.classList.toggle('is-active')
+    })
+}
