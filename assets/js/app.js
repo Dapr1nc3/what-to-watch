06e76a1083397b5d2be138dc9a50b782a9251c8b
Brian Done <briguy2078@gmail.com>
2021-11-29 20:52:01 -0500

    removed extra comments in js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,4 @@
-
-
-
-
-// Modal Save
+// Modal Save Button
 $("#input").click(function() {
 
     // close modal
@@ -18,7 +14,7 @@ $("#input").click(function() {
   
  });
 
- // Modal Close
+ // Modal Close Button
 $("#close").click(function() {
  
     $(".modal").removeClass("is-active");
@@ -29,64 +25,6 @@ $("#close").click(function() {
 
 
 
-// // Brians vanilla javascript start
-// var getMovies = function() {
-//     // The Movie Database API Link
-//     // Link pulls response in json in placed into browser directly
-//     // Still working on having the data show in console.log
-//     // Documentation URL: https://developers.themoviedb.org/3/getting-started/search-and-query-for-details
-//     // API image documention: https://developers.themoviedb.org/3/movies/get-movie-images
-//     // Sample API Movie Image Request: https://api.themoviedb.org/3/movie/550/images?/7PzJdsLGlR7oW4J0J5Xcd0pHGRg.png?api_key=f7d85a51bfa2246df462b8756b8f2df8&language=en-US
-//     // Sampe API Fight Club Movie Request: https://api.themoviedb.org/3/movie/550?api_key=f7d85a51bfa2246df462b8756b8f2df8
-//     var apiTmdbUrl = "https://api.themoviedb.org/3/movie/550?api_key=f7d85a51bfa2246df462b8756b8f2df8";
-
-//     // Make a Request to the TMDb API Link
-//     fetch(apiTmdbUrl).then(function(response) {
-//         response.json().then(function(data) {
-//             console.log(data);
-//         })
-//     });
-
-//     // YouTube API documention: https://developers.google.com/youtube/v3/getting-started
-//     var apiYoutubeUrl = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyBwjtqScAQ_N-v_ZExCLGwF7GHHh6CLfKQ";
-
-//     // Make a Request to the YouTube API Link
-//     fetch(apiYoutubeUrl).then(function(response) {
-//         response.json().then(function(data) {
-//             console.log(data);
-//         })
-//     });
-// };
-
-// // Initialize all div with carousel class
-// var carousels = bulmaCarousel.attach('.carousel');
-
-// // Loop on each carousel initialized
-// for(var i = 0; i < carousels.length; i++) {
-// 	// Add listener to  event
-// 	carousels[i].on('before:show', state => {
-// 		console.log(state);
-// 	});
-// }
-
-// // Access to bulmaCarousel instance of an element
-// var element = document.querySelector('#carousel-demo');
-// if (element && element.bulmaCarousel) {
-// 	// bulmaCarousel instance is available as element.bulmaCarousel
-// 	element.bulmaCarousel.on('before-show', function(state) {
-// 		console.log(state);
-// 	});
-// }
-
-// bulmaCarousel.attach('#carousel-demo', {
-//     slidesToScroll: 1,
-//     slidesToShow: 1,
-//     navigationKeys: true,
-    
-
-// });
-
-// Brians vanilla javascript end
 
 
 
